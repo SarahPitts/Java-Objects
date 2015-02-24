405881dd05d5c078e86f9b3da00c5d8a8e61184b
sarahpitts <mccallsmedia@gmail.com>
2015-02-24 14:02:18 -0600

    added notes from repl lesson

--- Example.java
+++ Example.java
@@ -1,3 +1,7 @@
+//java-repl will start the read evaluate print loop
+// :cls will clear the screen
+//.contains is case sensitive, must use .toLowerCase with it
+
 public class Example {
     
     public static void main(String[] args) {
