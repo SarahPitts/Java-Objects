0f180d337425aabed042b81387109080300b7852
sarahpitts <mccallsmedia@gmail.com>
2015-02-28 14:44:05 -0600

    worked with fields on newly created objects and classes

--- Example.java
+++ Example.java
@@ -7,6 +7,14 @@ public class Example {
     
     public static void main(String[] args) {
         // Your amazing code goes here...
-        
+        // System is a class that exposes a static public field named out
+        // out is a print string which exposes methods
+        // we can write to screen using println which automatically prints a new line making /n unnecessary
+        System.out.println("We are making a new Pez dispenser");
+        //just like every other variable this one will have a type.  it's type will be PezDispenser
+        // the name of the variable is dispenser
+        PezDispenser dispenser = new PezDispenser ();
+        //we can access the fields that we exposed publicly such as mCharacterName
+        System.out.printf("The dispenser character name is %s \n", dispenser.mCharacterName);
     }
 }
---
+++ PezDispenser.java
@@ -0,0 +1,7 @@
+//when defining a new class you will need an access level modifier
+public class PezDispenser {
+	//adds some state with a field or member variable
+	// public ensures that everyone who creates this object can see it
+	// member variables commonly begin with the letter m
+	public String mCharacterName = "Yoda";
+}
